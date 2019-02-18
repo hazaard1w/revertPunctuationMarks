d46acd31f7619d0515e0dc7dfa72ad7a76783d8a
Вадим Кондратьевв <kondratev.v@roistat.com>
2019-02-18 12:09:46 +0300

    fix

--- index.php
+++ index.php
@@ -6,4 +6,7 @@ use punctuation\{PunctRevertManager, IPunctRevertManager};
 /** @var IPunctRevertManager $punctRevertManager */
 $punctRevertManager = new PunctRevertManager();
 
+
+//////////////
+
 echo $punctRevertManager->revertPunctuationMarks("Привет! Как твои дела?");
